1312bd99668bcf11cd4551772fff5f5ca7be199f
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2019-12-08 02:41:30 +0300

    upload file with 6202 problem
    program is written in pascal

---
+++ USE programming/PascalABC/6202.pas
@@ -0,0 +1,38 @@
+﻿var
+begin
+
+  max7 := 0;
+  max := 0;
+  k := 0;
+  
+  while true do
+  begin
+    readln(x);
+    
+    if x = 0 then
+      break;
+    
+    k := k + 1;
+    
+    if (x mod 49 <> 0) and (x mod 7 = 0) and (x > max7) then
+      max7 := x;
+      
+    if (x mod 7 <> 0) and (x > max) then
+      max := x;      
+  end;
+  
+  if max * max7 = 0 then
+    R := 1
+  else
+    R := max * max7;
+  
+  writeln(M);
+  writeln('Введено чисел: ', k);
+  writeln('Контрольное значание: ', M);
+  writeln('Вычисленное контрольное значание: ', R);
+  if M = R then
+    writeln('Значения совпали')
+  else
+    writeln('Значения не совпали');
+    
+end.

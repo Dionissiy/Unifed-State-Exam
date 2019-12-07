fa593b50ac0b98b2378cc368571a00026e76120f
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2019-12-08 02:48:23 +0300

    update 6202.pas
    add comments

--- USE programming/PascalABC/6202.pas
+++ USE programming/PascalABC/6202.pas
@@ -1,31 +1,35 @@
 ﻿var
 begin
-
-  max7 := 0;
-  max := 0;
-  k := 0;
+  //initialize variables
+  max7 := 0;//max among positive numbers divisible by 7, but not by 49
+  max := 0;//max among positive numbers indivisible by 7
+  k := 0;//amount of numbers
   
+  //endless cycle with terminator 0
   while true do
   begin
-    readln(x);
-    
-    if x = 0 then
+    readln(x); //input a number
+   
+    if x = 0 then //terminator
       break;
     
-    k := k + 1;
+    //the main algorithm below
+    k := k + 1; 
     
     if (x mod 49 <> 0) and (x mod 7 = 0) and (x > max7) then
       max7 := x;
       
     if (x mod 7 <> 0) and (x > max) then
       max := x;      
-  end;
+  end; //end of cycle body
   
+  //computing the control value
   if max * max7 = 0 then
     R := 1
   else
     R := max * max7;
   
+  //form the report
   writeln(M);
   writeln('Введено чисел: ', k);
   writeln('Контрольное значание: ', M);
